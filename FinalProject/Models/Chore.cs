dd113ea017761595fccef5cf6393a765140d368a
Aayush Patel <aayushptl2005@gmail.com>
2023-03-07 10:10:01 -0600

    Initial Commit

---
+++ FinalProject/Models/Chore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FinalProject.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Models
+{
+	public enum Category
+	{
+		Cleaning,
+		Errand,
+		Finance,
+		Kids,
+		Shopping,
+		Bills
+	}
+
+	public enum Recurrence
+	{
+		Once,
+		Daily,
+		Weekly,
+		Monthly,
+		SemiMonthly,
+		Annualy
+	}
+
+	public class Chore
+	{
+		public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string Name { get; set; }
+		public DateTime DueDate { get; set; }
+
+        public Category? Category { get; set; }
+		public Recurrence Recurrence { get; set; }
+        public bool? Completed { get; set; }
+
+		public virtual User? User { get; set; }
+	}
+}
---
+++ FinalProject/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace FinalProject.Models
+{
+	public class User : IdentityUser
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public virtual ICollection<Chore> Chores { get; set; }
+	}
+}
